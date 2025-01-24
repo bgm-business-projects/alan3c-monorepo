8e15ae625f49db9e885b28445a816ab70f2e011e
Cool Man <bgm.mackiller@gmail.com>
2025-01-24 20:45:14 +0800

    fix: 修復 dialog 換頁無法移除 bug

--- apps/alan3c-web/components/base-login-dialog.vue
+++ apps/alan3c-web/components/base-login-dialog.vue
@@ -1,26 +1,32 @@
 <template>
-  <q-dialog ref="dialogRef" @hide="onDialogHide">
+  <q-dialog ref="dialogRef" persistent @hide="onDialogHide">
     <q-card class="q-dialog-plugin p-1rem">
       <q-form
         @submit="handleLogin"
       >
-        <div class="flex flex-col gap-1.5rem p-.7rem">
+        <div class="flex flex-col gap-1rem p-.7rem">
+          <div class="text-lg font-medium">
+            此頁面需驗證身份
+          </div>
+          <div class="text-#666">
+            請輸入帳號密碼
+          </div>
           <q-input
             v-model="account"
             label="帳號"
             lazy-rules
             outlined
-            dense
           />
           <q-input
             v-model="password"
             label="密碼"
             lazy-rules
             outlined
-            dense
           />
         </div>
-        <q-btn label="送出" type="submit" />
+        <div class="p-.7rem">
+          <q-btn label="送出" type="submit" outline />
+        </div>
       </q-form>
     </q-card>
   </q-dialog>
@@ -43,7 +49,7 @@ const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginC
 // 處理登入
 async function handleLogin() {
   emit('submit-credentials', {
-    account: account.value,
+    account: `${account.value}@gmail.com`,
     password: password.value,
   })
 }
--- apps/alan3c-web/pages/home/bibliography/index.vue
+++ apps/alan3c-web/pages/home/bibliography/index.vue
@@ -1,111 +1,112 @@
 <template>
   <div class="w-full flex flex-col gap-2rem items-center layout-padding py-1.5rem lg:py-3rem">
-    {{ bibliography }}
-    <!-- <div class="flex max-width">
-      <base-breadcrumbs
-        :bread-list="[
-          {
-            name: t('navbar.home'),
-            route: {
-              name: 'home',
+    <template v-if="bibliography">
+      <div class="flex max-width">
+        <base-breadcrumbs
+          :bread-list="[
+            {
+              name: t('navbar.home'),
+              route: {
+                name: 'home',
+              },
             },
-          },
-          {
-            name: t('home.bibliography'),
-            route: {
-              name: 'home-bibliography',
+            {
+              name: t('home.bibliography'),
+              route: {
+                name: 'home-bibliography',
+              },
             },
-          },
-        ]"
-      />
-    </div>
-    <div class="max-width flex flex-col gap-2rem">
-      <h1 class="text-2xl font-bold text-primary">
-        {{ t('home.bibliography') }}
-      </h1>
-    </div>
-    <div class="max-width flex flex-col gap-1rem">
-      <template v-if="!isLoading">
-        <template v-if="!bibliography">
-          <div
-            class="max-width bg-#f4f4f4 flex justify-center py-10rem rounded-.5rem font-medium text-lg text-#666"
-            :class="locale === 'zh' ? ['tracking-.1rem']
-              : locale === 'en' ? ['tracking-.05rem']
-                : []"
-          >
-            {{ t('notFound') }}
-          </div>
-        </template>
-        <template v-else>
-          <nuxt-link
-            :to="localePath({
-              name: 'home-bibliography-international-journal-papers',
-            })"
-            class="border bg-#FDFDFD rounded-.5rem px-1rem lg:px-2rem py-1rem tracking-.1rem flex lg:items-center gap-1rem flex-col lg:flex-row items-start"
-          >
-            <div class="flex-1 font-semibold text-lg">
-              {{ t('bibliography.internationalJournalPapers') }}
+          ]"
+        />
+      </div>
+      <div class="max-width flex flex-col gap-2rem">
+        <h1 class="text-2xl font-bold text-primary">
+          {{ t('home.bibliography') }}
+        </h1>
+      </div>
+      <div class="max-width flex flex-col gap-1rem">
+        <template v-if="!isLoading">
+          <template v-if="!bibliography">
+            <div
+              class="max-width bg-#f4f4f4 flex justify-center py-10rem rounded-.5rem font-medium text-lg text-#666"
+              :class="locale === 'zh' ? ['tracking-.1rem']
+                : locale === 'en' ? ['tracking-.05rem']
+                  : []"
+            >
+              {{ t('notFound') }}
             </div>
-            <div class="flex items-center gap-.4rem font-semibold border-solid border-black border-1px px-1rem py-.3rem rounded-.5rem">
-              <div>{{ t('moreInfo') }}</div>
-              <q-icon name="img:/arrow.svg" />
-            </div>
-          </nuxt-link>
-          <div v-for="item, index in bibliography.normal" :key="index">
+          </template>
+          <template v-else>
             <nuxt-link
               :to="localePath({
-                name: 'home-bibliography-id',
-                params: {
-                  id: item[0],
-                },
+                name: 'home-bibliography-international-journal-papers',
               })"
               class="border bg-#FDFDFD rounded-.5rem px-1rem lg:px-2rem py-1rem tracking-.1rem flex lg:items-center gap-1rem flex-col lg:flex-row items-start"
             >
               <div class="flex-1 font-semibold text-lg">
-                {{ t(`bibliography.${item[0]}`) }}
+                {{ t('bibliography.internationalJournalPapers') }}
               </div>
               <div class="flex items-center gap-.4rem font-semibold border-solid border-black border-1px px-1rem py-.3rem rounded-.5rem">
                 <div>{{ t('moreInfo') }}</div>
                 <q-icon name="img:/arrow.svg" />
               </div>
             </nuxt-link>
-          </div>
-          <nuxt-link
-            :to="localePath({
-              name: 'home-bibliography-authored-books',
-            })"
-            class="border bg-#FDFDFD rounded-.5rem px-1rem lg:px-2rem py-1rem tracking-.1rem flex lg:items-center gap-1rem flex-col lg:flex-row items-start"
-          >
-            <div class="flex-1 font-semibold text-lg">
-              {{ t('bibliography.authoredBooks') }}
+            <div v-for="item, index in bibliography.normal" :key="index">
+              <nuxt-link
+                :to="localePath({
+                  name: 'home-bibliography-id',
+                  params: {
+                    id: item[0],
+                  },
+                })"
+                class="border bg-#FDFDFD rounded-.5rem px-1rem lg:px-2rem py-1rem tracking-.1rem flex lg:items-center gap-1rem flex-col lg:flex-row items-start"
+              >
+                <div class="flex-1 font-semibold text-lg">
+                  {{ t(`bibliography.${item[0]}`) }}
+                </div>
+                <div class="flex items-center gap-.4rem font-semibold border-solid border-black border-1px px-1rem py-.3rem rounded-.5rem">
+                  <div>{{ t('moreInfo') }}</div>
+                  <q-icon name="img:/arrow.svg" />
+                </div>
+              </nuxt-link>
             </div>
-            <div class="flex items-center gap-.4rem font-semibold border-solid border-black border-1px px-1rem py-.3rem rounded-.5rem">
-              <div>{{ t('moreInfo') }}</div>
-              <q-icon name="img:/arrow.svg" />
-            </div>
-          </nuxt-link>
-          <nuxt-link
-            :to="localePath({
-              name: 'home-bibliography-submitted-papers',
-            })"
-            class="border bg-#FDFDFD rounded-.5rem px-1rem lg:px-2rem py-1rem tracking-.1rem flex lg:items-center gap-1rem flex-col lg:flex-row items-start"
-          >
-            <div class="flex-1 font-semibold text-lg">
-              {{ t('bibliography.submittedPapers') }}
-            </div>
-            <div class="flex items-center gap-.4rem font-semibold border-solid border-black border-1px px-1rem py-.3rem rounded-.5rem">
-              <div>{{ t('moreInfo') }}</div>
-              <q-icon name="img:/arrow.svg" />
-            </div>
-          </nuxt-link>
+            <nuxt-link
+              :to="localePath({
+                name: 'home-bibliography-authored-books',
+              })"
+              class="border bg-#FDFDFD rounded-.5rem px-1rem lg:px-2rem py-1rem tracking-.1rem flex lg:items-center gap-1rem flex-col lg:flex-row items-start"
+            >
+              <div class="flex-1 font-semibold text-lg">
+                {{ t('bibliography.authoredBooks') }}
+              </div>
+              <div class="flex items-center gap-.4rem font-semibold border-solid border-black border-1px px-1rem py-.3rem rounded-.5rem">
+                <div>{{ t('moreInfo') }}</div>
+                <q-icon name="img:/arrow.svg" />
+              </div>
+            </nuxt-link>
+            <nuxt-link
+              :to="localePath({
+                name: 'home-bibliography-submitted-papers',
+              })"
+              class="border bg-#FDFDFD rounded-.5rem px-1rem lg:px-2rem py-1rem tracking-.1rem flex lg:items-center gap-1rem flex-col lg:flex-row items-start"
+            >
+              <div class="flex-1 font-semibold text-lg">
+                {{ t('bibliography.submittedPapers') }}
+              </div>
+              <div class="flex items-center gap-.4rem font-semibold border-solid border-black border-1px px-1rem py-.3rem rounded-.5rem">
+                <div>{{ t('moreInfo') }}</div>
+                <q-icon name="img:/arrow.svg" />
+              </div>
+            </nuxt-link>
+          </template>
         </template>
-      </template>
-      <template v-else>
-        <div class="max-width flex items-center justify-center h-300px relative">
-          <q-inner-loading :showing="isLoading" />
-        </div>
-      </template>
-    </div> -->
+        <template v-else>
+          <div class="max-width flex items-center justify-center h-300px relative">
+            <q-inner-loading :showing="isLoading" />
+          </div>
+        </template>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -113,11 +114,11 @@
 import type { DialogChainObject } from 'quasar'
 import BaseLoginDialog from '../../../components/base-login-dialog.vue'
 
-// const { locale, t } = useI18n()
+const { locale, t } = useI18n()
 
 // const useBibliography = useBibliographyApi()
 
-// const localePath = useLocalePath()
+const localePath = useLocalePath()
 
 const $q = useQuasar()
 
@@ -141,7 +142,6 @@ async function loginAndGetData() {
   const [getDataError, getDataResult] = await to(authStore.fetchBibliography())
   if (getDataError) {
     isLoading.value = false
-    console.log('getDataError', getDataError)
     throw getDataError
   }
   isLoading.value = false
@@ -185,6 +185,18 @@ const { data: bibliography, refresh: refreshBibliography } = useLazyAsyncData('b
 onMounted(() => {
   isClient.value = true
 })
+
+onBeforeUnmount(() => {
+  console.log('bibliography', bibliography)
+  if (!bibliography.value) {
+    console.log('test')
+    dialog.value?.hide()
+  }
+})
+
+// onUnmounted(() => {
+//   dialog.value?.hide()
+// })
 // onMounted(async () => {
 //   if (bibliographyToken.value) {
 //     try {
