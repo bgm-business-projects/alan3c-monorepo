8542a2d3ea408a5fe6170453a1b743f2668be214
Cool Man <bgm.mackiller@gmail.com>
2025-04-01 12:13:40 +0800

    fix: 修復 internationalJournalPapers 日期排序
    #23

--- apps/alan3c-web/contract/international-journal-papers/get-list.contract.ts
+++ apps/alan3c-web/contract/international-journal-papers/get-list.contract.ts
@@ -8,6 +8,7 @@ export const getInternationalJournalPapers = {
   query: z.object({
     fields: z.string().default('*,file.*'),
     limit: z.number().default(9999),
+    sort: z.string().default('-statusDate'),
   }),
   responses: {
     200: internationalJournalPapersSchema,
